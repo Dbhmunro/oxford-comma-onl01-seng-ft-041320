54d40290b9dc0b1372069635deeb3d0bb35e88a6
David Munro <dbhmunro@gmail.com>
2020-04-16 19:44:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,4 @@
 def oxford_comma(array)
   array.length > 1 ? array[array.length-1] = "and #{array.last}" : nil
   array.length > 2 ? array.join(", ") : array.join(" ")
-  # case variations
-  #   when array.length == 1
-  #     array.join()
-  #   when array.length == 2
-  #     array.join(" and ")
-  # end
 end
